7537bd73929b150ba4ca60c7cc474fa168caff0f
almond <almondmendoza@gmail.com>
2020-05-27 16:42:56 +1000

    move pagination to components folder and rename maxNumberOnSide to paddingCount

--- src/components/pagination.vue
+++ src/components/pagination.vue
@@ -3,17 +3,17 @@
     <li v-if="this.currentPage !== 1" class="with-hover" >
       <a :href="url + '?page=1'" @click.prevent="gotoPage(currentPage - 1)" class="active">PREV</a>
     </li>
-    <li v-if="Math.max(this.currentPage - this.maxNumberOnSide, 1) === this.maxNumberOnSide"  :class="{active: currentPage === 1}" >
+    <li v-if="Math.max(this.currentPage - this.paddingCount, 1) === this.paddingCount"  :class="{active: currentPage === 1}" >
       <a :href="url + '?page=1'" @click.prevent="gotoPage(1)" class="active" data-page="1">1</a>
     </li>
-    <li v-if="Math.max(this.currentPage - this.maxNumberOnSide, 1) > this.maxNumberOnSide"><a @click.prevent="">...</a></li>
+    <li v-if="Math.max(this.currentPage - this.paddingCount, 1) > this.paddingCount"><a @click.prevent="">...</a></li>
 
     <li v-for="i in this.pagesInMiddle"  :class="{active: currentPage === i}" >
       <a :href="url + '?page=' + i" @click.prevent="gotoPage(i)">{{ i }}</a>
     </li>
 
-    <li v-if="Math.min(this.currentPage + this.maxNumberOnSide, this.lastPage) < this.lastPage - 1"><a @click.prevent="">...</a></li>
-    <li v-if="Math.min(this.currentPage + this.maxNumberOnSide, this.lastPage) < this.lastPage" :class="{active: currentPage === lastPage}">
+    <li v-if="Math.min(this.currentPage + this.paddingCount, this.lastPage) < this.lastPage - 1"><a @click.prevent="">...</a></li>
+    <li v-if="Math.min(this.currentPage + this.paddingCount, this.lastPage) < this.lastPage" :class="{active: currentPage === lastPage}">
       <a :href="url + '?page=' + this.lastPage" @click.prevent="gotoPage(lastPage)" >{{ this.lastPage }}</a>
     </li>
 
@@ -29,7 +29,7 @@
    */
   export default {
     props: {
-      maxNumberOnSide: {
+      paddingCount: {
         type: Number,
         default: 2
       },
@@ -53,8 +53,8 @@
     },
     computed: {
       pagesInMiddle () {
-        let start = Math.max(this.currentPage - this.maxNumberOnSide, 1);
-        let end = Math.min(this.currentPage + this.maxNumberOnSide, this.lastPage);
+        let start = Math.max(this.currentPage - this.paddingCount, 1);
+        let end = Math.min(this.currentPage + this.paddingCount, this.lastPage);
         return  Array(end - start + 1).fill().map((_, idx) => start + idx);
       }
     }
